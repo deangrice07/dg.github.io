7404ebbf42cca859bd0686a7644489fb745eae06
dg <deangrice07@bigpond.com>
2020-01-11 15:23:03 +1100

    changes

--- Repository/script.module.dg/lib/resources/lib/indexers/episodes.py
+++ Repository/script.module.dg/lib/resources/lib/indexers/episodes.py
@@ -168,7 +168,7 @@ class seasons:
             data = urllib2.urlopen(url, timeout=30).read()
 
             zip = zipfile.ZipFile(StringIO.StringIO(data))
-            result = zip.read('%s.zip.xml' % 'en')
+            result = zip.read('%s.xml' % 'en')
             artwork = zip.read('banners.xml')
             zip.close()
 
@@ -182,7 +182,7 @@ class seasons:
                 data = urllib2.urlopen(url, timeout=30).read()
 
                 zip = zipfile.ZipFile(StringIO.StringIO(data))
-                result = zip.read('%s.zip.xml' % 'en')
+                result = zip.read('%s.xml' % 'en')
                 artwork = zip.read('banners.xml')
                 zip.close()
 
@@ -191,7 +191,7 @@ class seasons:
                 data = urllib2.urlopen(url, timeout=30).read()
 
                 zip = zipfile.ZipFile(StringIO.StringIO(data))
-                result2 = zip.read('%s.zip.xml' % lang)
+                result2 = zip.read('%s.xml' % lang)
                 zip.close()
             else:
                 result2 = result
@@ -1101,7 +1101,7 @@ class episodes:
                 data = urllib2.urlopen(url, timeout=10).read()
 
                 zip = zipfile.ZipFile(StringIO.StringIO(data))
-                result = zip.read('%s.zip.xml' % lang)
+                result = zip.read('%s.xml' % lang)
                 zip.close()
 
                 result = result.split('<Episode>')
@@ -1377,7 +1377,7 @@ class episodes:
                 data = urllib2.urlopen(url, timeout=10).read()
 
                 zip = zipfile.ZipFile(StringIO.StringIO(data))
-                result = zip.read('%s.zip.xml' % lang)
+                result = zip.read('%s.xml' % lang)
                 zip.close()
 
                 result = result.split('<Episode>')
