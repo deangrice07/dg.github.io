da6df48171461925015b16155951baa3b74ecfc8
deangrice07 <deangrice07@bigpond.com>
2021-07-16 14:38:17 +1000

    changes

--- Matrix/Repository/script.module.dgscrapers/lib/default.py
+++ Matrix/Repository/script.module.dgscrapers/lib/default.py
@@ -3,9 +3,9 @@
 import sys, base64
 import six
 from six.moves import urllib_parse
-from oathscrapers import sources_oathscrapers
-from oathscrapers.modules import control
-from oathscrapers import providerSources, providerNames
+from dgscrapers import sources_dgscrapers
+from dgscrapers.modules import control
+from dgscrapers import providerSources, providerNames
 
 
 params = dict(urllib_parse.parse_qsl(sys.argv[2].replace('?', '')))
@@ -21,7 +21,7 @@ if not sys.argv[0] == d:
     sys.exit()
 
 def ScraperChoice():
-    from oathscrapers import providerSources
+    from dgscrapers import providerSources
     sourceList = providerSources()
     control.idle()
     select = control.selectDialog([i for i in sourceList])
@@ -32,7 +32,7 @@ def ScraperChoice():
     control.openSettings('0.1')
 
 def ToggleProviderAll(enable):
-    from oathscrapers import providerNames
+    from dgscrapers import providerNames
     sourceList = providerNames()
     (setting, open_id) = ('true', '0.3') if enable else ('false', '0.2')
     for i in sourceList:
@@ -42,11 +42,11 @@ def ToggleProviderAll(enable):
     control.openSettings(open_id)
 
 
-if action == "oathscrapersettings":
-    control.openSettings('0.0', 'script.module.oathscrapers')
+if action == "dgscrapersettings":
+    control.openSettings('0.0', 'script.module.dgscrapers')
 
-elif mode == "oathscrapersettings":
-    control.openSettings('0.0', 'script.module.oathscrapers')
+elif mode == "dgscrapersettings":
+    control.openSettings('0.0', 'script.module.dgscrapers')
 
 
 elif action == "ScraperChoice":
@@ -59,13 +59,13 @@ elif mode == "ScraperChoice":
 
 elif action == "toggleAll":
     sourceList = []
-    sourceList = sources_oathscrapers.all_providers
+    sourceList = sources_dgscrapers.all_providers
     for i in sourceList:
         source_setting = 'provider.' + i
         control.setSetting(source_setting, params['setting'])
 #    xbmc.log('All providers = %s' % sourceList,2)
     control.sleep(200)
-    control.openSettings(query, "script.module.oathscrapers")
+    control.openSettings(query, "script.module.dgscrapers")
 
 
 elif action == "ToggleProviderAll":
@@ -74,46 +74,46 @@ elif action == "ToggleProviderAll":
 
 elif action == "toggleAllHosters":
     sourceList = []
-    sourceList = sources_oathscrapers.hoster_providers
+    sourceList = sources_dgscrapers.hoster_providers
     for i in sourceList:
         source_setting = 'provider.' + i
         control.setSetting(source_setting, params['setting'])
 #    xbmc.log('All Hoster providers = %s' % sourceList,2)
     control.sleep(200)
-    control.openSettings(query, "script.module.oathscrapers")
+    control.openSettings(query, "script.module.dgscrapers")
 
 
 elif action == "toggleAllForeign":
     sourceList = []
-    sourceList = sources_oathscrapers.all_foreign_providers
+    sourceList = sources_dgscrapers.all_foreign_providers
     for i in sourceList:
         source_setting = 'provider.' + i
         control.setSetting(source_setting, params['setting'])
 #    xbmc.log('All Foregin providers = %s' % sourceList,2)
     control.sleep(200)
-    control.openSettings(query, "script.module.oathscrapers")
+    control.openSettings(query, "script.module.dgscrapers")
 
 
 elif action == "toggleAllGreek":
     sourceList = []
-    sourceList = sources_oathscrapers.greek_providers
+    sourceList = sources_dgscrapers.greek_providers
     for i in sourceList:
         source_setting = 'provider.' + i
         control.setSetting(source_setting, params['setting'])
 #    xbmc.log('All Greek providers = %s' % sourceList,2)
     control.sleep(200)
-    control.openSettings(query, "script.module.oathscrapers")
+    control.openSettings(query, "script.module.dgscrapers")
 
 
 elif action == "toggleAllTorrent":
     sourceList = []
-    sourceList = sources_oathscrapers.torrent_providers
+    sourceList = sources_dgscrapers.torrent_providers
     for i in sourceList:
         source_setting = 'provider.' + i
         control.setSetting(source_setting, params['setting'])
 #    xbmc.log('All Torrent providers = %s' % sourceList,2)
     control.sleep(200)
-    control.openSettings(query, "script.module.oathscrapers")
+    control.openSettings(query, "script.module.dgscrapers")
 
 
 # elif action == "Defaults":
@@ -126,5 +126,5 @@ elif action == "toggleAllTorrent":
         # source_setting = 'provider.' + i
         # control.setSetting(source_setting, params['setting'])
     # control.sleep(200)
-    # control.openSettings(query, "script.module.oathscrapers")
+    # control.openSettings(query, "script.module.dgscrapers")
 
