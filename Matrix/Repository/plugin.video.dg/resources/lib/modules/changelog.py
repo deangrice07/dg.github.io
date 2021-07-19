d143c2b28e49201a3b8a0dabac71b50ae553df54
deangrice07 <deangrice07@bigpond.com>
2021-07-19 16:03:47 +1000

    changes

--- Matrix/Repository/plugin.video.dg/resources/lib/modules/changelog.py
+++ Matrix/Repository/plugin.video.dg/resources/lib/modules/changelog.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-	Venom Add-on
+	dg Add-on
 """
 
-from resources.lib.modules.control import addonPath, addonId, getVenomVersion, joinPath
+from resources.lib.modules.control import addonPath, addonId, getdgVersion, joinPath
 from resources.lib.windows.textviewer import TextViewerXML
 
 
 def get():
-	venom_path = addonPath(addonId())
-	venom_version = getVenomVersion()
-	changelogfile = joinPath(venom_path, 'changelog.txt')
+	dg_path = addonPath(addonId())
+	dg_version = getdgVersion()
+	changelogfile = joinPath(dg_path, 'changelog.txt')
 	r = open(changelogfile)
 	text = r.read()
 	r.close()
-	heading = '[B]DG -  v%s - ChangeLog[/B]' % venom_version
-	windows = TextViewerXML('textviewer.xml', venom_path, heading=heading, text=text)
+	heading = '[B]DG -  v%s - ChangeLog[/B]' % dg_version
+	windows = TextViewerXML('textviewer.xml', dg_path, heading=heading, text=text)
 	windows.run()
 	del windows
